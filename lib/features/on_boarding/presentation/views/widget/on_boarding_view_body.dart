36925576ed25468e1e86cd82516f91c2f2326438
ahmed amer <01099517574med@gmail.com>
2024-12-02 00:19:27 +0200

    add dots indicator

--- lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hup/core/utils/app_color.dart';
 import 'package:fruit_hup/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -6,9 +8,18 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Expanded(child: OnBoardingPageView()),
+        const Expanded(
+          child: OnBoardingPageView(),
+        ),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color: AppColors.primaryColor.withOpacity(.5),
+          ),
+        ),
       ],
     );
   }
