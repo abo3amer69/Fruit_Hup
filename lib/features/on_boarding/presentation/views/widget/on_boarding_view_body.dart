8805f97d2a552341918d2483e5dd76cc884d0f26
ahmed amer <01099517574med@gmail.com>
2024-12-02 19:43:49 +0200

    create custom buttom

--- lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hup/constants.dart';
 import 'package:fruit_hup/core/utils/app_color.dart';
+import 'package:fruit_hup/core/widget/custom_buttom.dart';
 import 'package:fruit_hup/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -19,6 +21,21 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
             color: AppColors.primaryColor.withOpacity(.5),
           ),
+        ),
+        const SizedBox(
+          height: 29,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: KHorizentalPadding,
+          ),
+          child: CustomButton(
+            onPressed: () {},
+            text: 'ابدأ الان',
+          ),
+        ),
+        const SizedBox(
+          height: 43,
         ),
       ],
     );
