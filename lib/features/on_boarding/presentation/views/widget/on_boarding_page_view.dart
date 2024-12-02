2f9b132604f5a8fbdfd9ac618158ef5e15303066
ahmed amer <01099517574med@gmail.com>
2024-12-02 22:49:27 +0200

    fixed issue visibility condition 'تخط'

--- lib/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart
@@ -15,9 +15,7 @@ class OnBoardingPageView extends StatelessWidget {
       controller: pageController,
       children: [
         PageViewItem(
-          isVisible:
-              (pageController.hasClients ? pageController.page!.round() : 0) ==
-                  0,
+          isVisible: true,
           image: Assets.imagesPageViewItem1Image,
           backgroundoimage: Assets.imagesPageViewItem1BackgroundImage,
           subtitle:
@@ -42,15 +40,13 @@ class OnBoardingPageView extends StatelessWidget {
             ],
           ),
         ),
-        PageViewItem(
-          isVisible:
-              (pageController.hasClients ? pageController.page!.round() : 0) !=
-                  0,
+        const PageViewItem(
+          isVisible: false,
           image: Assets.imagesPageViewItem2Image,
           backgroundoimage: Assets.imagesPageViewItem2BackgroundImage,
           subtitle:
               'نقدم لك أفضل الفواكه المختارة بعناية. اطلع على التفاصيل والصور والتقييمات لتتأكد من اختيار الفاكهة المثالية',
-          title: const Text(
+          title: Text(
             'ابحث و تسوق',
             textAlign: TextAlign.center,
             style: TextStyle(
