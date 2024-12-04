703d7cc61dccfbf9acfb23a94218b3bea9d007fc
ahmed amer <01099517574med@gmail.com>
2024-12-05 00:44:38 +0200

    add package dartz(either (failure & succes))

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,5 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hup/core/error/failure.dart';
 import 'package:fruit_hup/features/auth/domain/entites/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserEmailAndPassword(String email, String password);
+  Future<Either<Failure, UserEntity>> createUserEmailAndPassword(
+      String email, String password);
 }
