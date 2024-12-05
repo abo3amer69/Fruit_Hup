156a52d9705bff698afd3f3126fd1d48e461e781
ahmed amer <01099517574med@gmail.com>
2024-12-05 18:52:40 +0200

    create cubit and create get it and add plocprovider to signup view

---
+++ lib/core/services/git_it_services.dart
@@ -0,0 +1,13 @@
+import 'package:fruit_hup/core/services/firebase_auth_services.dart';
+import 'package:fruit_hup/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:fruit_hup/features/auth/domain/repos/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+  getIt.registerSingleton<AuthRepo>(AuthRepoImpl(
+    firebaseAuthServices: getIt<FirebaseAuthServices>(),
+  ));
+}
--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hup/core/services/git_it_services.dart';
 import 'package:fruit_hup/core/widget/custom_app_bar.dart';
+import 'package:fruit_hup/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruit_hup/features/auth/presentation/cubits/cubit/signup_cubit.dart';
 import 'package:fruit_hup/features/auth/presentation/views/widget/signup_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -7,9 +11,14 @@ class SignUpView extends StatelessWidget {
   static const routeName = 'Signup';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: const SignUpViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(
+        getIt<AuthRepo>(),
+      ),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: const SignUpViewBody(),
+      ),
     );
   }
 }
