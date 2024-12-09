fa544c27081ee67937153b52b9e00e3d1686b43a
ahmed amer <01099517574med@gmail.com>
2024-12-09 19:21:12 +0200

    implement add data from user to firebase firestore

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,1 +1,11 @@
-class FirestoreService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruit_hup/core/services/database_services.dart';
+
+class FirestoreService implements DatabaseServices {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  @override
+  Future<void> addData(
+      {required String path, required Map<String, dynamic> data}) async {
+    await firestore.collection(path).add(data);
+  }
+}
