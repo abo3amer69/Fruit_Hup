f8604678668735b525eecca56ea72cb187c868a3
ahmed amer <01099517574med@gmail.com>
2024-12-11 13:49:26 +0200

    create custombuttomnavigationbar part one

--- lib/features/home/presentation/views/home_views.dart
+++ lib/features/home/presentation/views/home_views.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hup/features/home/presentation/views/widget/custom_buttom_navigation_bar.dart';
 import 'package:fruit_hup/features/home/presentation/views/widget/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,6 +10,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      bottomNavigationBar: CustomButtomNavigationBar(),
       body: SafeArea(child: HomeViewBody()),
     );
   }
